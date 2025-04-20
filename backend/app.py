d1205860e80b92648fc7b8ef7db1ec019c332403
Leonardo Kleimpaul <leonardokleimpaul@gmail.com>
2025-04-20 15:49:17 -0300

    Adicionado projeto todo ao docker

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/api/hello')
+def hello():
+    return jsonify({"message": "Olá do Flask!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
