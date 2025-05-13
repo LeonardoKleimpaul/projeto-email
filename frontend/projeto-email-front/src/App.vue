c5a6fe852001401f495b2d81471c7ade6ab62760
Leonardo Kleimpaul <leonardokleimpaul@gmail.com>
2025-05-13 20:25:14 -0300

    Ajustado ;

--- frontend/projeto-email-front/src/App.vue
+++ frontend/projeto-email-front/src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import axios from 'axios'
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 
-const msg = ref('')
+const msg = ref('');
 
 onMounted(() => {
   axios.get('http://127.0.0.1:5000/api/email/hello')
     .then(res => {
-      console.log(res)
-      msg.value = res.data.message
+      console.log(res);
+      msg.value = res.data.message;
     })
     .catch(err => {
-      msg.value = 'Erro ao conectar com a API'
-      console.error(err)
+      msg.value = 'Erro ao conectar com a API';
+      console.error(err);
     })
-})
+});
 </script>
 
 <template>
