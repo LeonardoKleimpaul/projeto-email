ad366aaf29cc892e1cb79b6ddd506d3f6726f658
LuanTedesco <luanfelipe2040@gmail.com>
2025-06-04 22:22:14 -0300

    config cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from app.extensions import db, cors
+from flask_cors import CORS
 from .routes.email_routes import email_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     db.init_app(app)
     cors.init_app(app)
